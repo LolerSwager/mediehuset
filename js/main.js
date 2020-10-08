7d3c55fb780e1269d481094055814a738933b6b7
Ollie-hub <Oliverjuul9@gmail.com>
2020-10-08 14:30:12 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-let prevScrollpos = window.pageYOffset; 
-window.onscroll = function() {
+let prevScrollpos = window.pageYOffset;   // Opretter variabel 
+window.onscroll = function() {  // Opretter en if / else function når man scroller på siden 
 let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-100px";
+  if (prevScrollpos > currentScrollPos) { // hvis man scroller tilbage til forrige position 
+    document.getElementById("navbar").style.top = "0"; //setter NAV top tilbage til 0
+  } else { //hvis man scroller ned så sætter den på -100px på NAV top
+    document.getElementById("navbar").style.top = "-100px"; 
   }
-  prevScrollpos = currentScrollPos;
+  prevScrollpos = currentScrollPos; 
 }
